f4acb72c7ef59d49e65d1dcd4609665c23a488ea
aCarinato <alessandro.carinato@gmail.com>
2022-10-26 09:03:31 +0200

    TEST3 socket on vercel

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import { createServer } from 'http';
-import { Server } from 'socket.io'; // TEST socket on vercel
+import { Server } from 'socket.io';
 
 import connectDB from './helper/db/db.js';
 // routes
@@ -24,39 +24,39 @@ const io = new Server(httpServer, { cors: { origin: '*' } }); // TEST socket on 
 //   },
 // });
 
-// io.on('connection', (socket) => { // TEST socket on vercel
-//   socket.on('join', async ({ userId }) => {
-//     const users = await addUser(userId, socket.id);
+io.on('connection', (socket) => {
+  socket.on('join', async ({ userId }) => {
+    const users = await addUser(userId, socket.id);
 
-//     setInterval(() => {
-//       // filter out the current user
-//       socket.emit('connectedUsers', {
-//         users: users.filter((user) => user.userId !== userId),
-//       });
-//     }, 10000);
-//   });
+    setInterval(() => {
+      // filter out the current user
+      socket.emit('connectedUsers', {
+        users: users.filter((user) => user.userId !== userId),
+      });
+    }, 10000);
+  });
 
-//   socket.on('leave', async ({ userId }) => {
-//     const users = await removeUserOnLeave(userId, socket.id);
-//   });
+  socket.on('leave', async ({ userId }) => {
+    const users = await removeUserOnLeave(userId, socket.id);
+  });
 
-//   // socket.on('disconnect', () => {
-//   //   removeUser(socket.id);
-//   // });
+  // socket.on('disconnect', () => {
+  //   removeUser(socket.id);
+  // });
 
-//   // socket.on('msgSent', ({ arg1, arg2 }) => {
-//   //   console.log({ arg1, arg2 });
+  // socket.on('msgSent', ({ arg1, arg2 }) => {
+  //   console.log({ arg1, arg2 });
 
-//   //   socket.emit('msgReceived', {
-//   //     msg: `hi! I received a msg with 2 args: ${arg1} and ${arg2}`,
-//   //   });
-//   // });
+  //   socket.emit('msgReceived', {
+  //     msg: `hi! I received a msg with 2 args: ${arg1} and ${arg2}`,
+  //   });
+  // });
 
-//   socket.on('sendMsg', (msg) => {
-//     // console.log('Msg received through socket: ' + msg);
-//     io.emit('sendMsg', msg);
-//   });
-// });
+  socket.on('sendMsg', (msg) => {
+    // console.log('Msg received through socket: ' + msg);
+    io.emit('sendMsg', msg);
+  });
+});
 
 const port = process.env.PORT || 8000;
 
